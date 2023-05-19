d71a22afb329707974a9b8ae364547caacdc5e45
Isabelle-Almeida <isabelletais1@gmail.com>
2023-05-19 10:19:27 -0300

    React

---
+++ src/components/Soma.tsx
@@ -0,0 +1,26 @@
+//Todo componente deve ter a primeirra letra maiuscula
+//Todo componente deve ter uma função que retorna um HTML
+//Todo componente deve ser exportado
+//Todo componente deve retornar apenas um elemento pai HTML
+
+function Soma() {
+    return (
+        <div>
+            <h2>Componente da soma</h2>
+            <label>
+
+            </label>
+        </div>
+    
+   
+    );
+
+
+}
+
+// Criar duas caixas de texto
+// Criar um botão
+// Somar os dois valores
+// Exibir em alert e depois no HTML
+
+export default Soma;
